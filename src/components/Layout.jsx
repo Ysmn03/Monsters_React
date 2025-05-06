0da55f17b8d9f2cf6221855fb956d884072946ad
Yasmin <yasmeenalmohtaseb@gmail.com>
2025-05-06 16:20:57 +0300

    Change font type

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import {Outlet} from 'react-router-dom'
 import Navbar from './NavbarComponent/Navbar'
-
+import './Layout.css'
 export default function Layout(){
     return(
         <div className='layout'>
